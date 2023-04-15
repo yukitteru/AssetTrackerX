b775f6fbf8567f8d33786af306e9d44f47f2955a
Yukitteru <yukitteru.a@gmail.com>
2023-04-15 20:09:14 +0500

    add supabase.ts

---
+++ src/supabase.ts
@@ -0,0 +1,16 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { createClient } from '@supabase/supabase-js'
+import Constants from 'expo-constants';
+require('dotenv').config();
+
+const supabaseUrl = Constants.expoConfig?.extra?.supabaseUrl;
+const supabaseAnonKey = Constants.expoConfig?.extra?.supabaseKey;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+    auth: {
+        storage: AsyncStorage as any,
+        autoRefreshToken: true,
+        persistSession: true,
+        detectSessionInUrl: false,
+    },
+});
